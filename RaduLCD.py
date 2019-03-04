f6e637b4e796fe42819c871dcdd9aad0af0f1cc7
Radu Nitescu <42891792+Nurckye@users.noreply.github.com>
2019-03-04 22:28:02 +0200

    Update RaduLCD.py
    Changes:
    - LCD wont flicker anymore (Problems adding printOnLCD function in a loop, noob mistake)
    - System now reverts to available state after 1-2 minutes of trying to find a face.
    - It prints a welcome message after a succesful log.
    Needs to be changed/added:
    - get_time function, I need to get rid of all the if-else statements. At this moment it works, I will change it soon.
    - add support in MongoDB for remote access. After that I will create an android app to view the logs.
    - hopefully buy a bluetooth module (HC-05 probably) and a servo motor soon to make the system actually unlock something.

--- RaduLCD.py
+++ RaduLCD.py
@@ -1,102 +1,91 @@
-#LCD 16x2
-import RPi.GPIO as GPIO
-import os
-import socket
-import fcntl
-import struct
+# februarie 2019, Nitescu Radu, UPB Automatica si Calculatoare, ANUL 1
+# radu.nitescu35@gmail.com
+import cv2
+import numpy
 import time
-from time import gmtime, strftime
+import datetime
+import serial
+import RaduLCD
+import base64
+import pymongo
+#afisare Data
+def get_time( timp ): #!!! Change it - dictionary
+    if timp.tm_mon == 1: luna = "January"
+    elif timp.tm_mon == 2: luna = "February"
+    elif timp.tm_mon == 3: luna = "March"
+    elif timp.tm_mon == 4: luna = "April"
+    elif timp.tm_mon == 5: luna = "May"
+    elif timp.tm_mon == 6: luna = "June"
+    elif timp.tm_mon == 7: luna = "July"
+    elif timp.tm_mon == 8: luna = "August"
+    elif timp.tm_mon == 9: luna = "September"
+    elif timp.tm_mon == 10: luna = "October"
+    elif timp.tm_mon == 11: luna = "November"
+    elif timp.tm_mon == 12: luna = "Decembrie"
+    if timp.tm_hour < 10 : ora = '0'+ str(timp.tm_hour)
+    else: ora = str(timp.tm_hour)
+    return luna + ' ' + str(timp.tm_mday) + ', ' + ora + '.' + str(timp.tm_min)
 
-LCD_RS = 20
-LCD_E  = 21
-LCD_D4 = 6
-LCD_D5 = 13
-LCD_D6 = 19
-LCD_D7 = 26
+#baza de date
 
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["UsersAndLogs"]
+Users = mydb["Users"]
+Logs = mydb["Logs"]
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
+ok = 1
+while True:
+        if ok == 1:
+            RaduLCD.printOnLCD("Apropiati cardul","de cititor")
+            ok = 0
+        cod_rfid = str(ser.readline())
+        cod_rfid = cod_rfid[3:14]
+        print(cod_rfid)
+        if len(cod_rfid)>2:
+            time.sleep(1)
+            myquery = { "RFID": cod_rfid }
+            print(myquery)
+            rezultate_cautare = Users.find_one(myquery)
+            print(rezultate_cautare)
+            if rezultate_cautare != None:
+                print(rezultate_cautare)
+                RaduLCD.printOnLCD("Asezati-va in","fata camerei!")
+                ok = 1
+                time.sleep(2)
+                cap = cv2.VideoCapture(0)
+                face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+                timp_start =  time.localtime(time.time())
+                minutul_inceperii = timp_start.tm_min
+                RaduLCD.printOnLCD("Cautare fata..."," ")
+                while(minutul_inceperii + 2 >= time.localtime(time.time()).tm_min):
+                    ret, img = cap.read() #ia cate un frame pe rand
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # face imaginea grayscale
+                    faces = face_cascade.detectMultiScale(gray,scaleFactor = 1.05, minNeighbors=5) #primeste coordonatele fetelor si lungimile
 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
-
-
-def printOnLCD(mesaj1,mesaj2):
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-  GPIO.setup(LCD_E, GPIO.OUT)  # E
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
-  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-  lcd_init()
-  lcd_string(mesaj1,LCD_LINE_1)
-  lcd_string(mesaj2,LCD_LINE_2)
-
-def lcd_init():
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
-
-def lcd_byte(bits, mode):
-  GPIO.output(LCD_RS, mode)
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
-  lcd_toggle_enable()
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
-  lcd_toggle_enable()
-
-def lcd_toggle_enable():
-  time.sleep(E_DELAY)
-  GPIO.output(LCD_E, True)
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)
-  time.sleep(E_DELAY)
-
-def lcd_string(message,line):
-  message = str(message)
-  message = message.ljust(LCD_WIDTH," ")
-  lcd_byte(line, LCD_CMD)
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
-
-#def incercare():
-#    if __name__ == '__main__':
-        #main()
-    #  except KeyboardInterrupt:
-#        pass
-#      finally:
-    #    lcd_byte(0x01, LCD_CMD)
-#        lcd_string("Goodbye!",LCD_LINE_1)
-#        GPIO.cleanup()
+                    #IFUL CU LUAREA POZEI, Upload in LOG, deschidere usa
+                    if len(faces) != 0:
+                        localtime = time.localtime(time.time())
+                        data_pozei = get_time(localtime)
+                        numele_pozei = data_pozei + '.jpg'
+                        cv2.imwrite(numele_pozei, img )
+                        RaduLCD.printOnLCD("Img capturata", "cu succes!")
+                        with open(numele_pozei, "rb") as imageFile:
+                            poza_binar = base64.b64encode(imageFile.read())
+                        intrare_logs = {
+                        "data":data_pozei,
+                        "Nume":rezultate_cautare["Nume"],
+                        "Prenume":rezultate_cautare["Prenume"],
+                        "CNP":rezultate_cautare["CNP"],
+                        "Fotografie":poza_binar
+                        }
+                        inserat = Logs.insert_one(intrare_logs)
+                        time.sleep(3)
+                        RaduLCD.printOnLCD("Bun Venit",rezultate_cautare["Nume"][0] + ". " + rezultate_cautare["Prenume"])
+                        time.sleep(3)
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
+            else:
+                RaduLCD.printOnLCD("Acces respins!","") #printurile sunt pe LCD 16x2. A se verifica nr de caractere pt fiecare rand
+                time.sleep(3)
+                ok = 1
